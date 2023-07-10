94c0350326c5454f0675815396db6cf855155cbf
Aditya <aditya.pandey.241103@gmail.com>
2023-07-10 12:08:05 +0530

    frontend complete

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.set("view engine", "ejs") //If we are putting all are ejs files in views fol
 // app.set("views", path.resolve(__dirname, "views/ejs")) => If we are making another directory inside views folder
 
 //load assets 
-app.use('/css', express.static(path.resolve(__dirname, "assets/css")))
+app.use('/css', express.static(path.resolve(__dirname, "assets/css/")))
 app.use('/img', express.static(path.resolve(__dirname, "assets/img")))
 app.use('/js', express.static(path.resolve(__dirname, "assets/js")))
 
@@ -29,6 +29,14 @@ app.use('/js', express.static(path.resolve(__dirname, "assets/js")))
 
 app.get('/', (req, res) => {
     res.render("index")
+})
+
+app.get('/add-user', (req, res) => {
+    res.render("add_user")
+})
+
+app.get('/update-user', (req, res) => {
+    res.render("update_user")
 })
 
 app.listen(3000, () => {
