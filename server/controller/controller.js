02cf3f832bc794c7169d37ab1696285996144599
Aditya <aditya.pandey.241103@gmail.com>
2023-07-10 18:42:59 +0530

    create user API

---
+++ server/controller/controller.js
@@ -0,0 +1,40 @@
+var Userdb = require('../model/model')
+
+// Create and save new user
+exports.create = (req, res) => {
+    //Validate request 
+    if (!req.body) {
+        res.status(400).send({ message: "Content can not be empty" });
+        return
+    }
+    const user = new Userdb({
+        name: req.body.name,
+        email: req.body.email,
+        gender: req.body.gender,
+        staytus: req.body.status,
+    })
+
+    // save user in the databasse
+    user.save(user).then((data) => {
+        res.send(data)
+    }).catch((err) => {
+        res.status(500).send({
+            message: err.message || "Some error Occured while create operation"
+        })
+    })
+}
+
+// Retrieve and return all users / Reetrieve only a single user
+exports.find = (req, res) => {
+
+}
+
+// Update a new identified user by user id
+exports.update = (req, res) => {
+
+}
+
+//Delete a user with specified user id in the request
+exports.delete = (req, res) => {
+
+}
--- server/routes/router.js
+++ server/routes/router.js
@@ -3,6 +3,7 @@ const route = express.Router()
 
 const services = require('../services/render')
 
+const controller = require('../controller/controller')
 
 /* 
   @description Home Route
@@ -26,5 +27,11 @@ route.get('/update-user', services.update_user)
 route.get('/update-user', (req, res) => {
     res.render("update_user")
 })
+
+//API ROUTE
+route.post('/api/users', controller.create)
+route.get('/api/users', controller.find)
+route.put('/api/users/:id', controller.update)
+route.delete('/api/users/:id', controller.delete)
 
 module.exports = route
