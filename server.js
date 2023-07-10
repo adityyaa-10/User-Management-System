4b9dab5a4d94e67a3c407e0312a27a0a35c5f5b2
Aditya <aditya.pandey.241103@gmail.com>
2023-07-10 11:18:39 +0530

    main page design

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ const express = require('express')
 const dotenv = require('dotenv')
 const app = express();
 const morgan = require('morgan')
+const bodyparser = require('body-parser')
+
+const path = require('path')
 
 dotenv.config({ path: 'config.env' })
 const PORT = process.env.PORT || 8080
@@ -10,8 +13,22 @@ const PORT = process.env.PORT || 8080
 // morgan module allows us to log a request on the console whenever a request is made.
 app.use(morgan('tiny'))
 
+// Parse request to body-parser
+app.use(bodyparser.urlencoded({ extended: true }))
+
+//set view engine
+app.set("view engine", "ejs") //If we are putting all are ejs files in views folder, then we need not to specify path
+// app.set("views", path.resolve(__dirname, "views/ejs")) => If we are making another directory inside views folder
+
+//load assets 
+app.use('/css', express.static(path.resolve(__dirname, "assets/css")))
+app.use('/img', express.static(path.resolve(__dirname, "assets/img")))
+app.use('/js', express.static(path.resolve(__dirname, "assets/js")))
+
+
+
 app.get('/', (req, res) => {
-    res.send("User Management System")
+    res.render("index")
 })
 
 app.listen(3000, () => {
